429e6ec35072e9ea67194b6451438def919ce067
An Tr <andrecee.tnn@gmail.com>
2021-05-17 12:47:51 +0700

    passed

--- src/main/java/DataReplace/DataReplace/ReplaceString.java
+++ src/main/java/DataReplace/DataReplace/ReplaceString.java
@@ -12,19 +12,17 @@ import com.opencsv.exceptions.CsvException;
 public class ReplaceString {	
 	
 	public static void main(String[] args) throws IOException, CsvException {
-		readCSV("E:\\test\\newInput.csv");
+		String[] headers = readCSV("E:\\test\\newInput.csv");
 		readTXT("E:\\test\\data\\e.txt");
-		writeXLSX("E:\\test\\output\\out.xlsx");
+		writeXLSX("E:\\test\\output\\out.xlsx", headers);
 	}
 	
-	public static void readCSV(String file) throws IOException, CsvException {
+	public static String[] readCSV(String file) throws IOException, CsvException {
 		CSVReader reader = new CSVReader( new FileReader(file));
 		List<String[]> allRows;
 		allRows = reader.readAll();
-		String[] header = allRows.get(0);
-		String a1 = header[0];
-		System.out.println(a1);
-	}
+		return allRows.get(0);
+		}
 	
 	public static void readTXT(String file) throws IOException {
 		FileReader fr = new FileReader("E:\\test\\data\\e.txt");
@@ -45,20 +43,18 @@ public class ReplaceString {
 		writer.close();
 	}
 	
-	public static void writeXLSX(String file) throws IOException {
+	public static void writeXLSX(String file, String[] headers) throws IOException {
 		XSSFWorkbook wb = new XSSFWorkbook( new FileInputStream(file));
 		XSSFSheet sheet = wb.getSheet("Sheet1");
 		XSSFRow row = null;		
 		int i = 0;
 		while((row = sheet.getRow(i)) != null){
 			Cell cell = row.createCell(0);
-			cell.setCellValue(a1);
+			cell.setCellValue(headers[i]);
 			i++;
-			System.out.print(i);
 }
 		FileOutputStream fileout = new FileOutputStream("E:\\test\\output\\new.xlsx");
 		wb.write(fileout);
-		System.out.print(fileout);
 	}		
 }
 	
